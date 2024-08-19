c1336363a4f005fa945c0dd0dd12907ba1c6e57f
Felix the cat <di09paula@gmail.com>
2024-08-19 15:35:34 -0300

    setup.Installation.sh

--- setup.Installation.sh
+++ setup.Installation.sh
@@ -6,6 +6,16 @@ apt upgrade
 apt full-upgrade
 #instalar sudo & nano
 apt install sudo nano adduser -y
-adduser 
 
+# Perguntar ao usuário qual nome de usuário ele quer
+echo "Digite o nome de usuário:"
+read username
+
+# Perguntar ao usuário qual senha ele quer
+echo "Digite a senha:"
+read -s password
+
+# Criar o usuário com o nome e senha fornecidos
+adduser --disabled-password --gecos "" "$username"
+echo "$username:$password" | chpasswd
 
